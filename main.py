aab12da64dcceca969b8b5512acdae4ab7ffd31e
veryheavypickle <xavier.carroll01@gmail.com>
2022-04-15 02:36:03 +0200

    Made some updates on yodas, didn't touch this much

--- main.py
+++ main.py
@@ -1,53 +1,20 @@
-import json
 from iOSbackup import iOSbackup
+from yodas import Menu
 
 
-# Everything to get the required credentials
-def getKeys():
-    keysPath = "user.json"
-    keys = openJSON(keysPath)
-
-    if keys == {}:
-        keys = createKeys(keysPath)
-
-    return keys
-
-
-def createKeys(path):
-    keyTypes = ["encryptionPassword",
-                "decryptionKey"]
-    keys = {}
-    for keyType in keyTypes:
-        print("\nPaste the {0} below".format(keyType))
-        key = input("{}: ".format(keyType))
-        keys[keyType] = key
-
-    writeJSON(path, keys)
-    print("New keys file created!")
-    return keys
-
-
-def openJSON(path):
-    # Path should always contain the extension
-    try:
-        jsonFile = open(path)
-        file = json.load(jsonFile)
-        jsonFile.close()
-        return file
-    except FileNotFoundError:
-        return {}
-
-
-def writeJSON(path, data):
-    assert type(data) is dict
-    jsonFile = open(path, "w")
-    json.dump(data, jsonFile)
-    jsonFile.close()
-    print("Saved to path: {}".format(path))
+def getDeviceList():
+    devices = iOSbackup.getDeviceList()
+    for device in devices:
+        print(device)
 
 
 def main():
-    getKeys()
+    # menuItems = [{"Quit": exit}, {"List devices": listDevices}]
+    menuItems = [{"Quit": exit}, getDeviceList]
+    mainMenu = Menu(menuItems, title="Main Menu")
+    while True:
+        m = mainMenu.show()
+        print(m())
 
 
 if __name__ == '__main__':
